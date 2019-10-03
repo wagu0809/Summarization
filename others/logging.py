dc948e152185f59cf5f8930d0165b27ea709e868
wagu0809 <wagu0809@sina.com>
2019-10-03 22:58:51 +0300

    Update logging.py

--- others/logging.py
+++ others/logging.py
@@ -1,24 +1,11 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import
-
 import logging
 
 logger = logging.getLogger()
 
 
-def init_logger(log_file=None, log_file_level=logging.NOTSET):
-    log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
+def init_logger(log_path):
+    logging.basicConfig(filename=log_path, level=logging.DEBUG, format='%(asctime)s: %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p')
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_format)
-    logger.handlers = [console_handler]
-
-    if log_file and log_file != '':
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setLevel(log_file_level)
-        file_handler.setFormatter(log_format)
-        logger.addHandler(file_handler)
 
     return logger
