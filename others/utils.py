9c1e8cbac1850f3ba11e713a0c08029b8001d593
wagu0809 <wagu0809@sina.com>
2019-10-03 20:56:11 +0300

    Update utils.py

--- others/utils.py
+++ others/utils.py
@@ -4,58 +4,59 @@ import shutil
 import time
 
 from others import pyrouge
+# import pyrouge
 
 REMAP = {"-lrb-": "(", "-rrb-": ")", "-lcb-": "{", "-rcb-": "}",
          "-lsb-": "[", "-rsb-": "]", "``": '"', "''": '"'}
 
 
-def clean(x):
-    return re.sub(
-        r"-lrb-|-rrb-|-lcb-|-rcb-|-lsb-|-rsb-|``|''",
-        lambda m: REMAP.get(m.group()), x)
-
-
-def process(params):
-    temp_dir, data = params
-    candidates, references, pool_id = data
-    cnt = len(candidates)
-    current_time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
-    tmp_dir = os.path.join(temp_dir, "rouge-tmp-{}-{}".format(current_time, pool_id))
-    if not os.path.isdir(tmp_dir):
-        os.mkdir(tmp_dir)
-        os.mkdir(tmp_dir + "/candidate")
-        os.mkdir(tmp_dir + "/reference")
-    try:
-
-        for i in range(cnt):
-            if len(references[i]) < 1:
-                continue
-            with open(tmp_dir + "/candidate/cand.{}.txt".format(i), "w",
-                      encoding="utf-8") as f:
-                f.write(candidates[i])
-            with open(tmp_dir + "/reference/ref.{}.txt".format(i), "w",
-                      encoding="utf-8") as f:
-                f.write(references[i])
-        r = pyrouge.Rouge155(temp_dir=temp_dir)
-        r.model_dir = tmp_dir + "/reference/"
-        r.system_dir = tmp_dir + "/candidate/"
-        r.model_filename_pattern = 'ref.#ID#.txt'
-        r.system_filename_pattern = r'cand.(\d+).txt'
-        rouge_results = r.convert_and_evaluate()
-        print(rouge_results)
-        results_dict = r.output_to_dict(rouge_results)
-    finally:
-        pass
-        if os.path.isdir(tmp_dir):
-            shutil.rmtree(tmp_dir)
-    return results_dict
+# def clean(x):
+#     return re.sub(
+#         r"-lrb-|-rrb-|-lcb-|-rcb-|-lsb-|-rsb-|``|''",
+#         lambda m: REMAP.get(m.group()), x)
+#
+#
+# def process(params):
+#     temp_dir, data = params
+#     candidates, references, pool_id = data
+#     cnt = len(candidates)
+#     current_time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
+#     tmp_dir = os.path.join(temp_dir, "rouge-tmp-{}-{}".format(current_time, pool_id))
+#     if not os.path.isdir(tmp_dir):
+#         os.mkdir(tmp_dir)
+#         os.mkdir(tmp_dir + "/candidate")
+#         os.mkdir(tmp_dir + "/reference")
+#     try:
+#
+#         for i in range(cnt):
+#             if len(references[i]) < 1:
+#                 continue
+#             with open(tmp_dir + "/candidate/cand.{}.txt".format(i), "w",
+#                       encoding="utf-8") as f:
+#                 f.write(candidates[i])
+#             with open(tmp_dir + "/reference/ref.{}.txt".format(i), "w",
+#                       encoding="utf-8") as f:
+#                 f.write(references[i])
+#         r = pyrouge.Rouge155(temp_dir=temp_dir)
+#         r.model_dir = tmp_dir + "/reference/"
+#         r.system_dir = tmp_dir + "/candidate/"
+#         r.model_filename_pattern = 'ref.#ID#.txt'
+#         r.system_filename_pattern = r'cand.(\d+).txt'
+#         rouge_results = r.convert_and_evaluate()
+#         print(rouge_results)
+#         results_dict = r.output_to_dict(rouge_results)
+#     finally:
+#         pass
+#         if os.path.isdir(tmp_dir):
+#             shutil.rmtree(tmp_dir)
+#     return results_dict
 
 
 def test_rouge(temp_dir, cand, ref):
     candidates = [line.strip() for line in open(cand, encoding='utf-8')]
     references = [line.strip() for line in open(ref, encoding='utf-8')]
-    print(len(candidates))
-    print(len(references))
+    print(f"the length of candidates {len(candidates)}")
+    print(f"the length of references {len(references)}")
     assert len(candidates) == len(references)
 
     cnt = len(candidates)
@@ -111,6 +112,7 @@ def tile(x, count, dim=0):
     if dim != 0:
         x = x.permute(perm).contiguous()
     return x
+
 
 def rouge_results_to_str(results_dict):
     return ">> ROUGE-F(1/2/3/l): {:.2f}/{:.2f}/{:.2f}\nROUGE-R(1/2/3/l): {:.2f}/{:.2f}/{:.2f}\n".format(
